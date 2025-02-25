2998ce14f9e64249c1e0fd61c5df659bb8aaca0b
zeynab jalalian <zjalalianweb@gmail.com>
2025-02-25 13:55:38 +0330

    js part2

--- script.js
+++ script.js
@@ -9,6 +9,21 @@ document.addEventListener("DOMContentLoaded",()=>{
      const[row2,col2]=[Math.floor(index2/3),index2 % 3];
      return(Math.abs(row1 - row2) + Math.abs(col1 - col2))===1;
     }
+    const swapCells=(cell1,cell2)=>{
+        [cell1.innerHTML,cell2.innerHTML]=[cell2.innerHTML,cell1.innerHTML];
+        cell1.classList.toggle("empty");
+        cell2.classList.toggle("empty");
+    }
+    const getAdjancentIndices=index=>{
+        const [row,col]=[Math.floor(index/3),index%3];
+        return [
+         row > 0 ? index - 3:null,
+         row < 2 ? index + 3:null,
+         row > 0 ? index - 1:null,
+         row < 2 ? index + 1:null
+        ].filter(n => n!==null);
+    };
+  
     
     
 })
